6be41faced6a1271c8ec49354cc4af02440028f6
Rscime <Rachelscime@gmail.com>
2023-09-27 20:07:48 -0400

    updated links and button styles

--- client/src/components/Signup.js
+++ client/src/components/Signup.js
@@ -119,7 +119,7 @@ export default function SignUp() {
             </button>
             <Grid container justifyContent="flex-end">
               <Grid item>
-                <Link href="#" variant="body2">
+                <Link href="/Login" variant="body2">
                   Already have an account? Sign in
                 </Link>
               </Grid>
